81339a8448fbd3987a71b76a8891af0b161b8f15
mudassir-arif <mudassir@aptechgdn.net>
2024-01-31 14:49:03 +0500

    slider is ready

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
 import Image from "next/image";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <>
     <Navbar/>
     <Main/>
+    {/* <Slider/> */}
     <Footer/>
     </>
       );
